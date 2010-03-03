5bd5adaf4a9d187d5186973d4808ae712899148a
Peter Higgins <dante@adcsugr.local>
2010-03-03 09:54:53 -0500

    updated to use placeAt into a bordercontainer, and no need for dojo.byId

--- js/PhotoBrowser/_base.js
+++ js/PhotoBrowser/_base.js
@@ -7,29 +7,30 @@ dojo.require('PhotoBrowser.Results');
 dojo.require('dijit.layout.BorderContainer');
 
 dojo.addOnLoad(function() {
+    
+    var bc = new dijit.layout.BorderContainer({ 
+		design : 'headline',
+		style : 'width:100%; height:100%'
+	}, 'container');
+	console.log('container set up');
+	
 	new PhotoBrowser.Toolbar({ 
 		region : 'top',
 		style : 'height:50px'
-	}, dojo.byId('toolbar'));
+	}, 'toolbar').placeAt(bc);
 	console.log('toolbar set up');
 	
 	new PhotoBrowser.StoredSearchController({ 
 		region : 'left', 
 		style : 'width:100px;', 
 		splitter : true 
-	}, dojo.byId('search_terms'));
+	}, 'search_terms').placeAt(bc);
 	console.log('search_terms set up');
 
 	new PhotoBrowser.Results({ 
 		region : 'center'
-	}, dojo.byId('results'));
+	}, 'results').placeAt(bc);
 	console.log('results set up');
-	
-	var bc = new dijit.layout.BorderContainer({ 
-		design : 'headline',
-		style : 'width:100%; height:100%'
-	}, dojo.byId('container'));
-	console.log('container set up');
-	
+
 	bc.startup();
 });
